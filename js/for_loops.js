23a4ac37e556cbc682e88b0836fadf458fe62060
monicacholico <monicachol@hotmail.com>
2018-03-23 14:58:43 -0500

    exercise break and continue

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,3 +1,5 @@
+// Multiplication exercise
+
 function showMultiplicationTable(number){
 
     for (var i = 1; i < 11; i++){
@@ -10,8 +12,8 @@ showMultiplicationTable(5);
 
 // Odd or Even exercise for loop
 
-var randomNumbers = Math.floor(Math.random() * 20) + 200;
-var isForCones = Math.floor(Math.random() * 5) + 1;
+// var randomNumbers = Math.floor(Math.random() * 20) + 200;
+// var isForCones = Math.floor(Math.random() * 5) + 1;
 
 for (var i = Math.floor(Math.random() * 20) + 180; i < 200; i++){
     if(i % 2 === 0){
@@ -41,5 +43,37 @@ for (var i = Math.floor(Math.random() * 20) + 180; i < 200; i++){
 //             (console.log ((9)i)))
 //     }
 // }
-//
 
+// Brian Exercise piramide
+
+
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+// 7777777
+// 88888888
+// 999999999
+
+for(var i = 1; i <=9; i++) {
+    i = i.toString();
+    console.log(i.repeat(i));
+}
+
+
+
+for (var i = 100; i > 0; i = i - 5){
+    console.log(i);
+}
+//
+//
+// Break and Continue Exercise
+
+for (var i = 1; i <= 50; i++) {
+    if (i % 2 === 0) {
+        continue;
+    }
+    console.log("Here's and odd number: " + i);
+}
