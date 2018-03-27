c8739281f303c74c258bedfade8add87b1571296
monicacholico <monicachol@hotmail.com>
2018-03-27 17:45:55 -0500

    tiding up code

--- js/external.js
+++ js/external.js
@@ -1,16 +1,18 @@
 "use strict";
 
 // Code here
-// console.log("Hello from external JavaScript");
-//
-// alert("Welcome to my website");
-// var color = prompt("What's your favorite color?");
-//
-// alert(" Wow, " + color + " is my favorite color too!!!" );
+
+console.log("Hello from external JavaScript");
+
+alert("Welcome to my website");
+
+var color = prompt("What's your favorite color?");
+
+alert(" Wow, " + color + " is my favorite color too!!!" );
 
 //This is the other exercise
 
-//Exercise 1
+// ================= EXERCISE 1 =====================================
 
 var littleMermaid = prompt("How many days do you have Little Mermaid?");
 
@@ -23,25 +25,35 @@ var total = (littleMermaid * 3) + (brotherBear * 3) + (hercules * 3);
 alert("Ok! your total is  $" + total);
 
 
-//Exercise 2
+//===================== EXERCISE 2 ==================================
 
-// var Google = prompt("How many hours did you work for Google");
-// var Amazon = prompt("How many hours did you work for Amazon");
-// var Facebook = prompt("How many hours did you work for Facebook");
-//
-// var total = Google * 400 + Amazon * 380 + Facebook * 350;
-//
-// alert("0k, then you'll total to be paid is " + "$" + total);
+var Google = prompt("How many hours did you work for Google");
+var Amazon = prompt("How many hours did you work for Amazon");
+var Facebook = prompt("How many hours did you work for Facebook");
 
-//Exercise 3
+var total = Google * 400 + Amazon * 380 + Facebook * 350;
+
+alert("0k, then you'll total to be paid is " + "$" + total);
+
+//===================== EXERCISE 3 ====================================
 
 var classSpots = prompt("Is there any spot available at this class?");
-// Zach var classSpots = prompt("Is the class full?") ==="yes";
+
 
 var freeSchedule = prompt("Do you have available schedule for this class?");
-//Zach var freeSchedule = prompt("Are there any schedule conflicts?") == "yes";
+
 
 var enrolled = classSpots && freeSchedule;
+
+
+alert("Ok, you are enrolled now");
+
+
+
+// ============================= ZACHS' SOLUTIONS IN CLASS ==========================
+
+// Zach var classSpots = prompt("Is the class full?") ==="yes";
+//Zach var freeSchedule = prompt("Are there any schedule conflicts?") == "yes";
 
 //Zach var canEnroll = !classSpots && !freeSchedule;
 
@@ -49,7 +61,6 @@ var enrolled = classSpots && freeSchedule;
 // var yesFreeSchedule = True;
 // var noSpots = False;
 // var noFreeSchedule = False;
-alert("Ok, you are enrolled now");
 
 //Zach alert("Able to enroll: " + canEnroll);
 
@@ -59,13 +70,11 @@ alert("Ok, you are enrolled now");
 //  if (enrolled) {
 //      alert("Great! Now you are enrolled");
 //  }
-
-
 // else {
 //     alert("Sorry, you can't be enrolled");
 // }
 
-//Exercise 4
+// ==================== EXERCISE 4 ===============================
 var itemsBought = prompt("How many items are you buying?");
 var offerValid = prompt("Is the offer expired?");
 var premiumMembership = prompt("Are you a premium member?");
@@ -75,7 +84,7 @@ var offerApplied = itemsBought >=2 && offerValid || offerValid && premiumMembers
 
 alert("The offer has been applied");
 
-// //Zach
+// // ======================== EXERCISE ZACH'S SOLUTION ================================
 // var itemsBought = parseInt(promp("How many items were bought?"));
 // var isOfferValid = parseInt(promp("Is the offer valid?")) == "yes";
 // var isPremiumMember = parseInt(promp("Are you a premium member?")) =="yes";
