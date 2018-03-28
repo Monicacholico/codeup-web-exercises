74f673ee9a7b47800f4e80371ab2cee45dee0c71
monicacholico <monicachol@hotmail.com>
2018-03-27 20:00:34 -0500

    commenting and tiding up code

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,4 +1,5 @@
-// Multiplication exercise
+// ============================== FOOR LOOPS =============================
+// ============== MULTIPLICATION TABLE EXERCISE ==========================
 
 function showMultiplicationTable(number){
     var i;
@@ -10,7 +11,9 @@ function showMultiplicationTable(number){
 
 showMultiplicationTable(5);
 
-// Odd or Even exercise for loop
+
+
+// ======= ODD OR EVEN EXERCISE WITH FOR LOOP ======================
 
 // var randomNumbers = Math.floor(Math.random() * 20) + 200;
 // var isForCones = Math.floor(Math.random() * 5) + 1;
@@ -23,28 +26,9 @@ for (var i = Math.floor(Math.random() * 20) + 180; i < 200; i++){
     }
 }
 
-// for (var i = 1; i < 10; i++) {
-//     switch (i) {
-//         case i = 1:
-//             console.log ( =+i);
-//         case i = 2:
-//             (console.log ((2)i)))
-//         case i = 4:
-//             (console.log ((4)i)))
-//         case i = 5:
-//             (console.log ((5)i)))
-//         case i = 6:
-//             (console.log ((6)i)))
-//         case i = 7:
-//             (console.log ((7)i)))
-//         case i = 8:
-//             (console.log ((8)i)))
-//         case i = 9:
-//             (console.log ((9)i)))
-//     }
-// }
 
-// Brian Exercise piramide
+
+// ======== PIRAMIDE EXERCISE ===================
 
 
 // 1
@@ -62,6 +46,7 @@ for(var i = 1; i <=9; i++) {
     console.log(i.repeat(i));
 }
 
+// ========== DESCENDING MULTIPLES OF 5 FROM 100 TO 0
 
 
 for (var i = 100; i > 0; i = i - 5){
