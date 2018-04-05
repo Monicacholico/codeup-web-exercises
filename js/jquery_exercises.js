b80e3d4d0390b49ab541debb1fe006525461b8a4
monicacholico <monicachol@hotmail.com>
2018-04-05 16:22:53 -0500

    finished jquery selector exercises

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,7 +1,97 @@
+(function() {
 
-"use strict";
+    "use strict";
 
-$(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
+// $(document).ready(function() {
+//     alert( 'The DOM has finished loading!' );
+//
+// });
+//
+// var content = $(".first-title").text();
+//     alert(content);
 
-});
+    // $('.codeup').css('border', 'solid 1px red');
+
+    $('li').css({
+        'font-size': '20px',
+        "list-style-tipe" : "circle"
+    });
+
+    $('li, p, h1').css('background-color', '#c6a59d');
+
+    var content = $("h1").text();
+    alert(content);
+
+    $("div").css("width", "600px");
+    // $("div").css("margin", "auto");
+    $("*").css({
+    "text-align": "justify",
+    "margin": "auto",
+    "padding": "10px"
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
