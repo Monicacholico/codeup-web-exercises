eae53ca1713e7ecbd90bc1d7ae4e2069ebabf375
monicacholico <monicachol@hotmail.com>
2018-04-08 16:40:43 -0500

    deleted comments

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -8,9 +8,39 @@ $('dt').click(function() {
 });
 
 
-    $( "p" ).click(function() {
-        $( this ).toggleClass( "highlight" );
+
+
+// $('.text').hide();
+// $('.expander').click(function(){
+//     $('.text').slideToggle(500);
+// });
+
+
+// $('.expander').click(function(){
+//     $(this).parent().next().slideToggle(200);
+// };
+
+
+$("button").click(function(){
+    $('li').each(function(index){
+        if (index % 4 ===0 ) {
+            $(this).next().next().next().css('background-color', '#FF0');
+        }
     });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
