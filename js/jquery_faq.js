ec1d59996cdfa4c72ea4a946c6b4582a00405f40
monicacholico <monicachol@hotmail.com>
2018-04-09 13:49:40 -0500

    finished traversing exercises

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -21,15 +21,144 @@ $('dt').click(function() {
 // };
 
 
-$("button").click(function(){
+$("#list-button").click(function(){
     $('li').each(function(index){
-        if (index % 4 ===0 ) {
+        if (index % 4 === 0 ) {
             $(this).next().next().next().css('background-color', '#FF0');
         }
     });
 });
 
+// $("h3").click(function(){
+//     $("li").each(function(i){
+//         if(i % 4 === 0){
+//         $(this).css("font-weight", "bold");
+//         }
+//     })
+//     });
 
+    $("li").click(function(){
+        $("li").each(function(i){
+            if(i % 4 === 0){
+                $(this).css("color", "blue");
+            }
+        })
+    });
+
+
+
+    $("#swap-button1").click(function(){
+        $("img").each(function(i){
+            if(i === 0){
+                $("img").first().animate({
+                        marginLeft: "225px"},
+                    500);
+            }
+            if(i === 1){
+                $("#frame2").animate({
+                        marginLeft: "-450px"},
+                    500);
+            }
+            if( i === 2){
+                $("img").last().animate({
+                    marginLeft: "225px"},
+                    500);
+            }
+        })
+    });
+
+
+
+    $("#swap-button2").click(function(){
+        $("img").each(function(i){
+            if(i === 1){
+                $(this).animate({
+                        marginRight: "250px"},
+                    500);
+            }
+            if(i === 2){
+                $("img").last().animate({
+                        marginLeft: "-1175px"},
+                    500);
+            }
+            if( i === 0){
+                $("img").first().animate({
+                        marginRight: "225px"},
+                    500);
+            }
+        })
+    });
+
+    $("#swap-button3").click(function(){
+        $("img").each(function(i){
+            if(i === 0){
+                $("img").first().animate({
+                        marginRight: "225px"},
+                    500);
+            }
+            if(i === 1){
+                $("#frame2").animate({
+                        marginRight: "450px"},
+                    500);
+            }
+            if( i === 2){
+                $("img").last().animate({
+                        marginLeft: "-1575px"},
+                    500);
+            }
+        })
+    });
+
+    // =========== THIS IS FOR THE H1 =======================
+
+    $("span").click(function(){
+        $("h1").hide();
+    });
+
+    //============ THESE ARE THE FOR THE UL FACTS ============
+
+
+    $("h3").first().click(function(){
+        $("#yellowstone").toggle();
+    });
+
+    $("#seneca-falls").click(function(){
+        $("#seneca").toggle();
+    });
+
+    $("h3").last().click(function(){
+        $("#wrangell").toggle();
+    })
+
+
+    // =========== THIS IS FOR THE H2 FADE IN ================
+
+
+    $("#register").fadeIn(8000);
+
+
+
+
+
+    // ============ TRYING TO DO IT WITH TOGGLE ===========
+
+    // $("#swap-button1").click(function(){
+    //     $("img").first().animate({
+    //             marginRight: "-225px"},
+    //         500);
+    // });
+    //
+    // $("#swap-button2").click(function(){
+    //     $("#frame2").animate({
+    //         width: "toggle"},
+    //         500);
+    // });
+    //
+    // $("#swap-button3").click(function(){
+    //     $("img").last().replaceWith($("#frame1"))
+    //     && $("img").next().animate({
+    //             width:"toggle"});
+    // })
 
 
 
