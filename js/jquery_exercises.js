66255942bee9115f341d35222a5dc66e2ea024a0
monicacholico <monicachol@hotmail.com>
2018-04-26 08:33:21 -0500

    uncommented some lines

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -4,33 +4,33 @@
 
 // ============== FIRST JQUERY EXERCISE =======================
 
-// $(document).ready(function() {
-//     alert( 'The DOM has finished loading!' );
-//
-// });
+$(document).ready(function() {
+    alert( 'The DOM has finished loading!' );
+
+});
 
 
 // ======= SECOND JQUERY EXERCISE "SELECTORS" =====================
 //
-// var content = $(".first-title").text();
-//     alert(content);
+var content = $(".first-title").text();
+    alert(content);
 
-    // $('.codeup').css('border', 'solid 1px red');
+    $('.codeup').css('border', 'solid 1px red');
 
-    // $('li').css({
-    //     'font-size': '20px',
-    //     "list-style-tipe" : "circle"
-    // });
-    //
-    // $('li, p, h1').css('background-color', '#c6a59d');
-    //
-    // var content = $("h1").text();
-    // alert(content);
+    $('li').css({
+        'font-size': '20px',
+        "list-style-tipe" : "circle"
+    });
+
+    $('li, p, h1').css('background-color', '#c6a59d');
+
+    var content = $("h1").text();
+    alert(content);
 
     alert($("h1").text());
-    //
-    // $("div").css("width", "600px");
-    // // $("div").css("margin", "auto");
+
+    $("div").css("width", "600px");
+    $("div").css("margin", "auto");
     $("*").css({
     "text-align": "justify",
     "margin": "auto",
